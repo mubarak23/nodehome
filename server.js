4cd2c486d91ca253178bfcdeda0a644bf4fc45e1
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-12-12 09:45:20 +0100

    setup mlab with mongoose

--- server.js
+++ server.js
@@ -3,19 +3,45 @@ var mongoose = require('mongoose');
 
 var app = new express();
 
+mongoose.connect('mongodb://root:root123@ds251240.mlab.com:51240/nodehome', 
+	function(err){
+		if(err){
+			console.log(err);
+		}else{
+			console.log('Connected to the DB')
+		}
+	});
+
+var UserSchema = new mongoose.Schema({
+	name: String,
+	age: Number
+});
+
+var User = mongoose.model('User', UserSchema);
 
 app.get('/', function(req, res, next){
 	res.json('Home page');
+});
+
+app.get('/create_user', function(req, res, next){
+	var user = new User();
+		user.name = "mubarak";
+		user.age = 123456;
+		user.save(function(err){
+			if(err) next(err);
+			res.json(user);
+		});
+		
 });
 
 app.get('/category', function(req, res, next){
 	res.json('All Categories are Displayed Here');
 });
 
-app.get('/:name', function(req, res, next){
+/*app.get('/:name', function(req, res, next){
 		res.json(req.params.name);
 });
-
+*/
  function enterkenya(req, res, next){
  	if(req.params.name == "ok"){
  		next();

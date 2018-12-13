485c9c315046719b9027f31593f321b0f4d15608
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-12-13 08:10:04 +0100

    add signin feature

--- passport.js
+++ passport.js
@@ -10,3 +10,23 @@ passport.serialUser(function(user, done){
 passport.deserializeUser(function(id, done){
 	done(err, user);
 })
+
+//Sign in
+passport.use('local-login', new LocalStrategy({
+	usernameField: 'email',
+	passwordField: 'pasword',
+	passReqToCallback: true
+}, function(req, email, password, done){
+		User.findOne({email: email}, function(err, user){
+			if(err){
+				return done(err);
+			}
+			if(!user){
+				return done(null, false);
+			}
+			if(!user.comparePassword(password)){
+				return done(null, false);
+			}
+			return done(null, user);
+		});
+}))
